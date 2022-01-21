0c974d5d7f4d501eed289776f4f23e4a4abca028
Marcono1234 <Marcono1234@users.noreply.github.com>
2022-01-21 22:58:18 +0100

    Suppress javadoc warnings for missing documentation

--- serial-builder-codegen/build.gradle.kts
+++ serial-builder-codegen/build.gradle.kts
@@ -33,6 +33,22 @@ tasks.test {
     jvmArgs = listOf("--add-opens", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED")
 }
 
+tasks.javadoc {
+    options {
+        // Cast to standard doclet options, see https://github.com/gradle/gradle/issues/7038#issuecomment-448294937
+        this as StandardJavadocDocletOptions
+
+        encoding = "UTF-8"
+        // Enable doclint, but ignore warnings for missing tags, see
+        // https://docs.oracle.com/en/java/javase/17/docs/specs/man/javadoc.html#additional-options-provided-by-the-standard-doclet
+        // The Gradle option methods are rather misleading, but a boolean `true` value just makes sure the flag
+        // is passed to javadoc, see https://github.com/gradle/gradle/issues/2354
+        addBooleanOption("Xdoclint:all,-missing", true)
+    }
+
+    shouldRunAfter(tasks.test)
+}
+
 tasks.jar {
     manifest {
         attributes["Main-Class"] = "marcono1234.serialization.serialbuilder.codegen.Main"
